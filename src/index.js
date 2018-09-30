33b54766244a87d8f5272a3da2c2c80a7ea5b0be
MariannaGarbuzova <vesna09@tut.by>
2018-09-30 22:15:57 +0300

    solved partly

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  var result = 0;
+  var twosCount = 0;
+  var fivesCount = 0;
+  for (let currentNumber = 2; currentNumber < number; currentNumber++) {
+    twosCount = twosCount + getCount(currentNumber, 2);
+    fivesCount = fivesCount + getCount(currentNumber, 5);
+  }
+
+  result = Math.min(twosCount, fivesCount);
+
+  return result;
 }
+
+function getCount(number, devider) {
+  var count = 0;
+  var reminder = 0;
+  //var number = number;
+  //Math.floor(reminder/coin.value)
+  while (reminder == 0 && number >= devider) {
+
+    reminder = number % devider;
+    if (reminder == 0) {
+      count++;
+    }
+
+    number = Math.floor(number / devider);
+  }
+
+  return count;
+}
+
+//var res = module.exports(84583674);
+//console.log(res);
