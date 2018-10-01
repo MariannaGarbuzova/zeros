06f3c5319cb51c8497d85241ad5affebcca5baa8
MariannaGarbuzova <36666837+MariannaGarbuzova@users.noreply.github.com>
2018-10-01 10:11:41 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 module.exports = function getZerosCount(number) {
   var result = 0;
-  var twosCount = 0;
   var fivesCount = 0;
-  for (let currentNumber = 2; currentNumber < number; currentNumber++) {
-    fivesCount = fivesCount + getCount(currentNumber, 5);
+  for (let currentNumber = 5; currentNumber < number; currentNumber++) {
+    fivesCount = fivesCount + getCount(currentNumber);
   }
 
   result = fivesCount;
@@ -11,53 +10,19 @@ module.exports = function getZerosCount(number) {
   return result;
 }
 
-function getCount(number, devider) {
+function getCount(number) {
   var count = 0;
   var reminder = 0;
-  while (reminder == 0 && number >= devider) {
-
-    reminder = number % devider;
+  var mult = 5;
+  while(reminder == 0) {
+    reminder = number % mult;
     if (reminder == 0) {
       count++;
+      mult = mult * 5;
     }
-
-    number = Math.floor(number / devider);
-  }
+  }  
 
   return count;
 }
 
-function getTwosCount(number) {
-  var binStr = number.toString(2);
-  var zeroCount = 0;
-  for (let i = binStr.length - 1; i > 0; i--) {
-    var char = binStr[i];
-    if (char == "0")
-    {
-      zeroCount++;
-    }
-    else {
-      break;
-    }
-  }
-
-  return zeroCount;
-}
-
-
-function getFivesCount(number) {
-  var binStr = number.toString(5);
-  var zeroCount = 0;
-  for (let i = binStr.length - 1; i > 0; i--) {
-    var char = binStr[i];
-    if (char == "0")
-    {
-      zeroCount++;
-    }
-    else {
-      break;
-    }
-  }
-
-  return zeroCount;
-}
+//module.exports(84583674);
