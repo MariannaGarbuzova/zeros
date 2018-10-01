6a641ff4a9a1275036198a01d3fa7a0c9a2422b0
MariannaGarbuzova <36666837+MariannaGarbuzova@users.noreply.github.com>
2018-10-01 10:36:37 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 module.exports = function getZerosCount(number) {
   var result = 0;
   var fivesCount = 0;
-  for (let currentNumber = 5; currentNumber < number; currentNumber++) {
-    fivesCount = fivesCount + getCount(currentNumber);
-  }
+
+  fivesCount = fivesCount + getCount(number);  
 
   result = fivesCount;
 
@@ -12,17 +11,9 @@ module.exports = function getZerosCount(number) {
 
 function getCount(number) {
   var count = 0;
-  var reminder = 0;
-  var mult = 5;
-  while(reminder == 0) {
-    reminder = number % mult;
-    if (reminder == 0) {
-      count++;
-      mult = mult * 5;
-    }
-  }  
+  
+  for (var i = 5; number / i >= 1; i *= 5) 
+        count = count + Math.floor(number / i); 
 
   return count;
 }
-
-//module.exports(84583674);
